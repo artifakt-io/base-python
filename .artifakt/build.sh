b8c056d92baa4ee2799b9f83817a916ceff50cf4
djalal <djalal@users.noreply.github.com>
2021-06-07 18:45:11 +0200

    add official page (#2)
    * add official page
    
    * cleaning

--- .artifakt/build.sh
+++ .artifakt/build.sh
@@ -8,4 +8,20 @@ echo "The following build args are available:"
 env
 echo "------------------------------------------------------------"
 
+RUNTIME_NAME="Python"
+RUNTIME_LOGO="python.png"
+ARCHIVE_FILE=static-page-main
+
+env
+
+curl -sSLO https://gitlab.com/djalal/static-page/-/archive/main/${ARCHIVE_FILE}.tar.gz && \
+  tar -xzf ${ARCHIVE_FILE}.tar.gz -C /tmp && \
+  mv /tmp/${ARCHIVE_FILE}/* /var/www/html && \
+  chown -R www-data:www-data /var/www/html && \
+  rm ${ARCHIVE_FILE}.tar.gz
+
+sed -i "s/__RUNTIME_NAME__/${RUNTIME_NAME}/" /var/www/html/index.html
+sed -i "s/__RUNTIME_LOGO__/${RUNTIME_LOGO}/" /var/www/html/index.html
+
+
 echo ">>>>>>>>>>>>>> END CUSTOM BUILD SCRIPT <<<<<<<<<<<<<<<<< "
